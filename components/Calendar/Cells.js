d4b9da6e7acea56a1c41c214af148f732c0e675d
FMGordillo <facundomgordillo@gmail.com>
2019-02-21 12:47:08 -0300

    Split BIG component in smaller ones

---
+++ components/Calendar/Cells.js
@@ -0,0 +1,63 @@
+import dateFns from "date-fns"
+
+export default ({ matches, currentMonth, selectedDate }) => {
+  const monthStart = dateFns.startOfMonth(currentMonth)
+  const monthEnd = dateFns.endOfMonth(currentMonth)
+  const startDate = dateFns.startOfWeek(monthStart)
+  const endDate = dateFns.endOfWeek(monthEnd)
+
+  const compareFormat = "YYYY-MM-DD"
+
+  const dateFormat = "D"
+  const rows = []
+
+  let days = []
+  let day = startDate
+  let formattedDate = ""
+
+  while (day <= endDate) {
+    for (let i = 0; i < 7; i++) {
+      formattedDate = dateFns.format(day, dateFormat)
+      // const cloneDay = day
+      days.push(
+        <div
+          className={`col cell ${
+            !dateFns.isSameMonth(day, monthStart)
+              ? "disabled"
+              : dateFns.isSameDay(day, selectedDate)
+              ? "selected"
+              : ""
+          }`}
+          key={day}
+          // onClick={() => this.onDateClick(dateFns.parse(cloneDay))}
+        >
+          <span className="number">{formattedDate}</span>
+          <span className="bg">{formattedDate}</span>
+          <div className="matches">
+            {matches
+              .filter(match => {
+                const isEqual = dateFns.isEqual(
+                  dateFns.format(day, compareFormat),
+                  dateFns.format(match.reservation_date, compareFormat)
+                )
+                return isEqual
+              })
+              .map((match, i) => (
+                <p className="match" key={i}>
+                  {match.title}
+                </p>
+              ))}
+          </div>
+        </div>
+      )
+      day = dateFns.addDays(day, 1)
+    }
+    rows.push(
+      <div className="row" key={day}>
+        {days}
+      </div>
+    )
+    days = []
+  }
+  return <div className="body">{rows}</div>
+}
---
+++ components/Calendar/Header.js
@@ -0,0 +1,26 @@
+import { format } from "date-fns"
+import es from "date-fns/locale/es"
+export default ({ prevMonth, currentMonth, nextMonth }) => {
+  const dateFormat = "MMMM YYYY"
+  return (
+    <div className="header row flex-middle">
+      <div className="col col-start">
+        <div className="icon" onClick={prevMonth}>
+          ⏪
+        </div>
+      </div>
+      <div className="col col-center">
+        <span>
+          {format(currentMonth, dateFormat, {
+            locale: es
+          })}
+        </span>
+      </div>
+      <div className="col col-end">
+        <div className="icon" onClick={nextMonth}>
+          ⏩
+        </div>
+      </div>
+    </div>
+  )
+}
